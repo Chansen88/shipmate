ac525083e7bdb922767a2ea468759d1a209f6d02
Chris Hansen <Chansen88@me.com>
2016-01-27 10:59:36 -0800

    Set up for local testing

--- public/app/services/googleauth.js
+++ public/app/services/googleauth.js
@@ -6,7 +6,7 @@
       let GoogleAuth = {}
       const clientId = '600096885677-g29tqgs658lqsdrh5tcpqkhcip1b4v81.apps.googleusercontent.com';
       const scope = 'email https://www.googleapis.com/auth/gmail.readonly';
-      const redirectUri = 'https://nameless-gorge-4169.herokuapp.com/gmailauth/';
+      const redirectUri = 'http://localhost:3000/gmailauth/';
       const responseType = 'token';
       const url = 'https://accounts.google.com/o/oauth2/auth?scope=' +
       scope + '&client_id=' + clientId + '&redirect_uri=' + redirectUri +
